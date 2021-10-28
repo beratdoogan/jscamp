5b4fa4676076ec98c53196b4719dbdcdeef6a1d3
Berat Doğan <77922331+beratdoogan@users.noreply.github.com>
2021-10-28 23:57:19 +0300

    Part 1

---
+++ src/app.js
@@ -0,0 +1,27 @@
+console.log("merhaba outshifts");
+
+let dolarDun = 9.20;
+let dolarBugun = 9.30;
+// JS TypeSafe değildir.(Sen başta hangi tür tamamladıysan sonrasında değiştirebilirsin)
+{
+    let dolarDun = 9.10;
+}
+
+console.log(dolarDun);
+
+const euroDun = 11.2; //(sabit değişken olduğundan const la kurulanlar asla değeri değişmez) 
+
+console.log(euroDun);
+
+let konutKredileri = [12,["a","b","c","d"],"Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredileri"];
+
+console.log("<ul>");
+
+for(let i = 0; i<konutKredileri.length; i++){
+    console.log("<li>" + konutKredileri[i] + "</li>" );
+}
+
+
+console.log("</ul>");
+
+console.log(konutKredileri);
