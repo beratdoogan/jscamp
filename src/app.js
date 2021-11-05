507812740a0f08aceb85eb32efddeced7ece2051
Berat Doğan <77922331+beratdoogan@users.noreply.github.com>
2021-11-05 20:53:00 +0300

    version2

--- src/app.js
+++ src/app.js
@@ -1,27 +1,35 @@
-console.log("merhaba outshifts");
+// console.log("merhaba outshifts");
 
-let dolarDun = 9.20;
-let dolarBugun = 9.30;
-// JS TypeSafe değildir.(Sen başta hangi tür tamamladıysan sonrasında değiştirebilirsin)
-{
-    let dolarDun = 9.10;
-}
+//  let dolarDun = 9.20;
+//  let dolarBugun = 9.30;
+// //JS TypeSafe değildir.(Sen başta hangi tür tamamladıysan sonrasında değiştirebilirsin)
+//  {
+//     let dolarDun = 9.10;
+//  }
 
-console.log(dolarDun);
+//  console.log(dolarDun);
 
-const euroDun = 11.2; //(sabit değişken olduğundan const la kurulanlar asla değeri değişmez) 
+//  const euroDun = 11.2; //(sabit değişken olduğundan const la kurulanlar asla değeri değişmez) 
 
-console.log(euroDun);
+//  console.log(euroDun);
 
-let konutKredileri = [12,["a","b","c","d"],"Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredileri"];
+//  let konutKredileri = [12,["a","b","c","d"],"Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredileri"];
 
-console.log("<ul>");
+//  console.log("<ul>");
 
-for(let i = 0; i<konutKredileri.length; i++){
-    console.log("<li>" + konutKredileri[i] + "</li>" );
-}
+//  for(let i = 0; i<konutKredileri.length; i++){
+//     console.log("<li>" + konutKredileri[i] + "</li>" );
+//  }
 
 
-console.log("</ul>");
+//  console.log("</ul>");
 
-console.log(konutKredileri);
+//  console.log(konutKredileri);
+
+// let student = {id:1,name="Engin"}
+ 
+// function save(ogrenci){
+//     console.log(ogrenci);
+// }
+// save(student);
+
